fixed min-q-val computation bug

algorithms/baseline/Q_learning_ext/nnet.py
@@ -19,19 +19,19 @@ class NNet(object):
         actions = tf.placeholder(tf.int32, [None])
 
         # build the prediction graph
-        L = 100
-        M = 50
+        L = 50
+        M = 20
 
         states = tf.placeholder(tf.float32, [None, obssize])
         W1 = tf.Variable(tf.truncated_normal([obssize, L],stddev=0.1))
         B1 = tf.Variable(tf.truncated_normal([L], stddev=0.1))
-        
+
         W2 = tf.Variable(tf.truncated_normal([L, M],stddev=0.1))
         B2 = tf.Variable(tf.truncated_normal([M], stddev=0.1))
-        
+
         W3 = tf.Variable(tf.truncated_normal([M, actsize],stddev=0.1))
         B3 = tf.Variable(tf.truncated_normal([actsize], stddev=0.1))
-        
+
         Z1 = tf.nn.relu(tf.matmul(states, W1) + B1)
         Z2 = tf.nn.relu(tf.matmul(Z1, W2) + B2)
 

algorithms/baseline/training.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, '/Users/shatianwang/Desktop/Elevator-MARL-Environment')
 import environment as gym
 import random
 import time
@@ -51,14 +53,14 @@ for i in range(nElevator):
 
 # if resume training using prev trained model
 # Use the saver to restore trained model from disk
-ckpt_path = "./q_learning_ext_model.ckpt" # specify path to the checkpoint file
-saver = tf.train.Saver()
-saver.restore(sess, ckpt_path)
-print("Model restored.")
+# ckpt_path = "./q_learning_ext_model.ckpt" # specify path to the checkpoint file
+# saver = tf.train.Saver()
+# saver.restore(sess, ckpt_path)
+# print("Model restored.")
 ## ---------------------------------------------------
-## if train from scratch, switch to the code below
-#sess.run(tf.global_variables_initializer())
-#ckpt_path = "./q_learning_ext_model.ckpt" # specify path to the checkpoint file for model storage
+# if train from scratch, switch to the code below
+sess.run(tf.global_variables_initializer())
+ckpt_path = "./q_learning_ext_model.ckpt" # specify path to the checkpoint file for model storage
 ## ---------------------------------------------------
 
 
@@ -93,14 +95,16 @@ while env.now() <= simulation_hours * 3600:
         # Update NNet
         # extract Q values in current state
         q_vals = Q[agent].compute_Qvalues([ss[i]])[0]
+
+        # construct legal_actions_list, in ascending order of action index
+        legal_actions_list = list(env.legal_actions(agent))
+        legal_actions_list.sort()
+
         if prev_actions[agent] != None: # if not the first time being a decision agent
             # update corresponding NNet
             time_elapsed = ss[i][-1]
             # compute min Q value in current state
-            legal_action_binary = np.zeros(actsize)
-            for a in env.legal_actions(agent):
-                legal_action_binary[a] = 1
-            min_Q_val = np.amin(np.multiply(q_vals, legal_action_binary))
+            min_Q_val = np.amin(q_vals[legal_actions_list])
             # compute target Q value of PREVIOUS state, action pair
             target = R[i] + np.exp(-beta * time_elapsed) * min_Q_val
             # update agent's NNet parameters
@@ -122,12 +126,8 @@ while env.now() <= simulation_hours * 3600:
         if print_counter % 500 == 0:
             print_counter = 0
             print("action probability distribution", prob_dist)
-            print("Q_vals: ", q_vals)
+            print("Q_vals: ", q_vals[legal_actions_list])
             print("reward: ",R[i])
-
-        # match legal_actions_list's order with prob_dist's order
-        legal_actions_list = list(env.legal_actions(agent))
-        legal_actions_list.sort()
 
         # sample a legal action from prob_dist of legal actions
         action = np.random.choice(legal_actions_list, p = prob_dist)